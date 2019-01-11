c963d02bf6ebcd596a39de75d39b0bf1ca6c6339
Cheng Wang <wangcheng678@gmail.com>
2019-01-12 01:57:59 +0800

    fix: typo

--- src/options/widgets/About.tsx
+++ src/options/widgets/About.tsx
@@ -5,17 +5,17 @@ export default () => (
     <h5>About</h5>
     <p>
       This extension is built with{' '}
-      <a href="http://cycle.js.org/" terget="_blank">
+      <a href="http://cycle.js.org/" target="_blank">
         Cycle.js
       </a>{' '}
       and{' '}
-      <a href="http://getskeleton.com/" terget="_blank">
+      <a href="http://getskeleton.com/" target="_blank">
         Skeleton
       </a>
       . Source code is on{' '}
       <a
         href="https://github.com/wangcheng678/no-redir-browser-extension"
-        terget="_blank"
+        target="_blank"
       >
         Github
       </a>
